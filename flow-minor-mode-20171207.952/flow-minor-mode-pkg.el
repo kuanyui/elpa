1bc12b3fee73e9bd3922993336405b6e4afea2e1
onohiroko <azazabc123@gmail.com>
2018-02-10 16:19:14 +0800

    Lots of 3rd party package

---
+++ flow-minor-mode-20171207.952/flow-minor-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flow-minor-mode" "20171207.952" "Flow type mode based on web-mode." '((emacs "25.1")) :commit "640a99bdc5f5d484a546c41800255cd43ff710e6" :url "https://github.com/an-sh/flow-minor-mode")
