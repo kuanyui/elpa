8d53a60ce523d460c43a7d36274b86f31de2a32f
onohiroko <azazabc123@gmail.com>
2018-08-15 10:21:43 +0800

    Update some packages

---
+++ ialign-0.4.2/ialign-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ialign" "0.4.2" "visual align-regexp" '((emacs "24.4")) :commit "7ad88c8f7922adc616b8f060b65fa1add8952ea1" :keywords '("tools" "editing" "align" "interactive") :authors '(("Michał Kondraciuk" . "k.michal@zoho.com")) :maintainer '("Michał Kondraciuk" . "k.michal@zoho.com") :url "https://github.com/mkcms/interactive-align")
