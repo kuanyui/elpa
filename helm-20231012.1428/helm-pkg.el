49d0cde06e1aacb32347842733a38f9c94930c67
ono ono <azazabc123@gmail.com>
2023-10-13 16:00:24 +0800

    helm

---
+++ helm-20231012.1428/helm-pkg.el
@@ -0,0 +1,14 @@
+(define-package "helm" "20231012.1428" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.9.4")
+    (wfnames "1.1")
+    (popup "0.5.3"))
+  :commit "89baca1f8449baf4de99eaa799773417fcbae4f1" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
