fe0c083ffed98a6ec2d0c4c2d0d49ce1a767df3a
ono ono <azazabc123@gmail.com>
2024-03-11 16:14:03 +0800

    apparmor

---
+++ apparmor-mode-20240211.2243/apparmor-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "apparmor-mode" "20240211.2243" "Major mode for editing AppArmor policy files"
+  '((emacs "26.1"))
+  :commit "05a67cba7508cf63bed22763a8949e55a7bf6786" :authors
+  '(("Alex Murray" . "murray.alex@gmail.com"))
+  :maintainers
+  '(("Alex Murray" . "murray.alex@gmail.com"))
+  :maintainer
+  '("Alex Murray" . "murray.alex@gmail.com")
+  :url "https://github.com/alexmurray/apparmor-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
